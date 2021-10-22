d3ad699ddc172ad5bd42fe99599e5400ef9ed223
Jordan <martinj1@franklinacademy.org>
2021-10-22 18:36:40 -0400

    large commit

--- TeamCode/src/main/java/LearnJava/HelloWorld.java
+++ TeamCode/src/main/java/LearnJava/HelloWorld.java
@@ -7,18 +7,19 @@ import com.qualcomm.robotcore.hardware.DistanceSensor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
+import DriveEngine.BadMecanumAuto;
+
 
 @Autonomous(name="Motor Test", group="LearnJava")
 public class HelloWorld extends LinearOpMode {
-    TankDrive driveBase;
+    BadMecanumAuto driveBase;
     DcMotor spinMotor;
     DistanceSensor distanceSensor;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        driveBase = new TankDrive(hardwareMap, this);
+        driveBase = new BadMecanumAuto(hardwareMap, this);
         spinMotor = hardwareMap.get(DcMotor.class, "Carousel");
-        distanceSensor = hardwareMap.get(DistanceSensor.class, "distance_sensor");
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -27,31 +28,31 @@ public class HelloWorld extends LinearOpMode {
         //Move forward
         driveBase.move(24, 12);
 
-        //Turn towards duck
-        driveBase.turnRight(75, 8);
-
-        //Push duck into box thing
-        driveBase.move(40, 12);
-
-        //Backward to turn toward Carosol
-        driveBase.move(-10, 7.5);
-
-        //Turn towards carosol
-        driveBase.turnRight(60, 12);
-
-        //Move towards wall to re angle
-        driveBase.move(22, 12);
-
-        //re angle against the wall
-        driveBase.turnRight(145, 8);
-
-        //angle towards carosol using wall
-            //----Set power right motor and wait 3 seconds
-        telemetry.addData("Distance from wall (inches)", distanceSensor.getDistance(DistanceUnit.INCH));
-        telemetry.update();
-
-        //move to carosol
-        driveBase.move(-6, 6);
+//        //Turn towards duck
+//        driveBase.turnRight(75, 8);
+//
+//        //Push duck into box thing
+//        driveBase.move(40, 12);
+//
+//        //Backward to turn toward Carosol
+//        driveBase.move(-10, 7.5);
+//
+//        //Turn towards carosol
+//        driveBase.turnRight(60, 12);
+//
+//        //Move towards wall to re angle
+//        driveBase.move(22, 12);
+//
+//        //re angle against the wall
+//        driveBase.turnRight(145, 8);
+//
+//        //angle towards carosol using wall
+//        telemetry.addData("Distance from wall (inches)", distanceSensor.getDistance(DistanceUnit.INCH));
+//        telemetry.update();
+//
+//        //move to carosol
+//        driveBase.move(-6, 6);
+//
 /*
         //Spin carosol
         spinMotor.setPower(1);
