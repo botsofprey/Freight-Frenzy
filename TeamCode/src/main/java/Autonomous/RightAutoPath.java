a2dc2b79519a2432d245e740ea2370542a689e7b
Jordan <martinj1@franklinacademy.org>
2022-02-02 17:54:12 -0500

    auto works

--- TeamCode/src/main/java/Autonomous/RightAutoPath.java
+++ TeamCode/src/main/java/Autonomous/RightAutoPath.java
@@ -2,11 +2,15 @@ package Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 import DriveEngine.NewMecanumDrive;
 import DriveEngine.SplineCurve;
 import Subsystems.CameraPipeline;
 import Subsystems.Carousel;
+import Subsystems.Intake;
 import Subsystems.Lift;
 import UtilityClasses.HardwareWrappers.Camera;
 import UtilityClasses.Location;
@@ -16,13 +20,16 @@ public class RightAutoPath extends LinearOpMode {
 	private NewMecanumDrive drive;
 	private Carousel carousel;
 	private Lift lift;
+	private Intake intake;
 
 	private Location carouselLocation = new Location(-18, -4, 0);
 	private Location corner1 = new Location(-18, -40, 0);
-	private Location shippingHub = new Location(7, -38, 90);
+	private Location shippingHub = new Location(7, -40, 90);
 	private Location corner2 = new Location(-18, -36, 90);
 	private Location corner3 = new Location(-18, -12, 90);
-	private Location ramPause = new Location(54, -12, -90);
+	private Location ramPause = new Location(44, -12, -90);
+	private Location warehouseEntrance = new Location(54, 12, -90);
+	private Location warehouse = new Location(60, 12, -90);
 
 	@Override
 	public void runOpMode() throws InterruptedException {
@@ -32,6 +39,7 @@ public class RightAutoPath extends LinearOpMode {
 				new Location(0, 0, 0), this);
 		carousel = new Carousel(hardwareMap, this, true);
 		lift = new Lift(hardwareMap, this, true);
+		intake = new Intake(hardwareMap, this, true);
 
 		while (!isStarted() && !isStopRequested()) {
 			drive.update();
@@ -78,11 +86,10 @@ public class RightAutoPath extends LinearOpMode {
 		sleep(200);
 		drive.rotate(-90);
 		sleep(200);
-		drive.moveToLocation(ramPause);
-		sleep(500);
-		drive.ram();
-		sleep(2000);
-		drive.brake();
-		while (opModeIsActive());
+		drive.moveToLocation(warehouseEntrance);
+		sleep(200);
+		drive.moveToLocation(warehouse);
+
+		while (opModeIsActive()) sleep(200);
 	}
 }
--- TeamCode/src/main/java/DriveEngine/NewMecanumDrive.java
+++ TeamCode/src/main/java/DriveEngine/NewMecanumDrive.java
@@ -297,7 +297,7 @@ public class NewMecanumDrive {
 //					(endTime - System.currentTimeMillis()) / 1000.0);
 //			mode.telemetry.update();
 			double h = hController.calculateAdjustment(currentLocation.getHeading());
-			if (currentLocation.headingDifference(angle) < 2
+			if (Math.abs(currentLocation.headingDifference(angle)) < 2
 					|| endTime <= System.currentTimeMillis()) {
 				rawMove(0, 0, 0);
 				break;
